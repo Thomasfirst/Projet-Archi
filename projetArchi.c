c680cf28fc7a103c854e49aee161c019a76b42cd
Thomasfirst <thomasroussarie2000@gmail.com>
2020-12-22 17:55:43 +0100

    stats

--- projetArchi.c
+++ projetArchi.c
@@ -2,7 +2,85 @@
 #include <stdlib.h>
 #include <string.h>
 
-	int main()
+
+void statisque(){
+    FILE* fichier = NULL;
+
+    fichier = fopen("test.txt", "r");
+
+    int TabInt[10] = {0};
+
+    char temp0[28];
+    char temp1[28];
+    char temp2[28];
+    char temp3[28];
+    char temp4[28];
+    char temp5[28];
+    char temp6[28];
+    char temp7[28];
+    char temp8[28];
+    char temp9[28];
+
+    char* TabChar[10] = {temp0,temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8,temp9};
+
+    char temp[28];
+
+    int tour = 0;
+    int existe = 0;
+    int min=99;
+    int indiceMin;
+   // char end[28]= "\0";
+
+    if (fichier == NULL){
+        printf("YOUSK2\n");
+    }
+    
+        //while (strcmp( temp, end ) != 0)
+        while( feof(fichier)==0 )
+        //for (int irr = 0; irr < 20; ++irr)
+        {
+            fscanf(fichier,"%s",temp);
+            for (int i = 0; i < 10; ++i)
+            {
+                if (strcmp( temp, TabChar[i] ) == 0)
+                {
+                    TabInt[i] ++;
+                    existe = 1;
+                }
+            }
+            if (existe == 0)
+            {
+                for (int j = 0; j < 10; ++j)
+                 {
+                    if (TabInt[j] < min )
+                    {
+                        min = TabInt[j];
+                        indiceMin = j;
+                    }
+                 }
+                 //TabChar[min] = temp;
+                 strcpy(TabChar[indiceMin],temp);
+                 TabInt[indiceMin] = 1;
+                
+            }
+
+            existe = 0;
+            tour++;
+            min =99;
+        }
+
+        for (int i = 0; i < 10; ++i)
+        {
+            printf("%d de %s \n",TabInt[i] ,TabChar[i]);
+        }
+
+    fclose(fichier);
+
+}
+
+
+
+int main()
 {
     FILE* fichier = NULL;
     int NbCaracLigne = 100;
@@ -73,5 +151,10 @@
         printf("Le fichier a %d lignes.\nEt il a %d pages.\nEt il a %d phrases\nEt il a %d mots.\n", NbLignes, NbPages, NbPhrases,NbMots);
     }
  
+
+    statisque();
+
     return 0;
 }
+
+//    java -jar Mars4_5.jar &
