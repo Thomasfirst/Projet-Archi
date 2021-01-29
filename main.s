a499384c24f6753d858dfea48ee2f00aaa8be584
rossithomaseise <71629347+rossithomaseise@users.noreply.github.com>
2021-01-29 11:31:24 +0100

    Add files via upload
    toute ma partie jusqu'à fermeture du fichier 2

--- main.s
+++ main.s
@@ -1,8 +1,11 @@
 	.data
 erreur_ouverture_fichier:	.asciiz "erreur ouverture fichier\n"
 erreur_ouverture_fichier2:	.asciiz "\nerreur ouverture fichier partie 2\n"
+erreur_ouverture_fichier3:	.asciiz "\nerreur ouverture fichier 3\n"
 success_ouverture_fichier:	.asciiz "succès ouverture fichier\n"
 success_ouverture_fichier2:	.asciiz "\nsuccès ouverture fichier partie 2\n"
+success_ouverture_fichier3:	.asciiz "\nsuccès ouverture fichier 3\n"
+texte_while:			.asciiz "\n ouais ca marche\n"
 nbr_de_phrases:			.asciiz "le nombre de phrase est: "
 nbr_mots:			.asciiz "le nombre de mot est: "
 nbr_de_lignes:			.asciiz "le nombre de lignes est: "
@@ -10,7 +13,7 @@ nbr_de_pages:			.asciiz "le nombre de pages est: "
 nbr_lignes_dans_page:		.word 6
 
 chemin_texte:                	.asciiz "/home/polytech/Téléchargements/Projet_Archi_final/Marseillaise.txt"
-chemin_texte2:                	.asciiz "/home/polytech/Téléchargements/Projet_Archi_final/Marseillaise.txt"
+chemin_texte2:                	.asciiz "/home/polytech/Téléchargements/Projet_Archi_final/texte2.txt"
 buffer:				.space 1
 tab_resultat_fonction:		.word 0,0,0,0
 
@@ -81,7 +84,7 @@ main:
   	
 	# On répète la procédure d'ouverture et de fermeture du fichier afin de lancer la fonction fréquence mot  	
   	li  $v0, 13            	 		# syscall pour l'ouverture du fichier
-        la  $a0,chemin_texte2  			# chemin du fichier
+        la  $a0,chemin_texte  			# chemin du fichier
         li  $a1,0        	 		# lecture = 0 (flag)
         li  $a2,0        	 		# = 0 (mode : reading from standard input)
         syscall        		 		# ouverture du fichier
@@ -348,6 +351,8 @@ f7_frequence_mot:
 	sw $fp,4($sp)
 	addu $fp,$sp,8
 	
+	#ATTENTION $t0 et $t7 réservé, s7 et s5 pour caractère précédent et caractère actuel
+	
 	la $t7,tab_resultat_fonction		# chargement nb mots texte
 	lw $t7,4($t7)
 	ori $t0,$zero,0				# initialisation i alias big (code C)
@@ -365,15 +370,338 @@ f7_frequence_mot:
 	bge $t1,$t5,fin_initialisation_temp_f7
 	mul $t3,$t2,$t1				# 4*i
 	add $t4,$t6,$t3				# récupère adresse temp[i]
-	lw $t4,0($t4)				# récupère valeur de l'adresse temp[i]
-	ori $t4,$zero,0				# initialisation à zéro
-	
-	
+	lw $s1,0($t4)				# récupère valeur de l'adresse temp[i]
+	ori $s1,$zero,0				# initialisation à zéro
+	sw $s1,0($t4)
 	addi $t1,$t1,1				# incrémentation 2ème for init temp
 	j initialisation_temp_f7		# retour 2ème for alias	
 	
 	fin_initialisation_temp_f7:		# fin initialisation temp
+	# initialisation loop
+	la $t6,loop				# chargement de l'entier loop
+	lw $s1,0($t6)
+	ori $s1,$zero,0				# initialisation loop à zéro
+	sw $s1,0($t6)	
+			
+	# lecture caractère
+        li $v0,14				# code service lecture fichier
+        
+        # chargement des paramètres
+        ori $a0,$s6,0				# adresse fichier
+        la $a1,buffer				# adresse buffer
+        ori $a2,$zero,1				# nombre de caractère lue
+        syscall
+        
+        ori $s7,$v0,0				# copie du caractère lu dans Caractère_précédent
+        
+       	# 3ème for
+	ori $t1,$zero,0				# initialisation i alias i (code C)	
+	addi $t2,$zero,4			# valeur 4
 	
+	ori $t3,$zero,0x21			# caract '!'
+	ori $t4,$zero,0x2E			# caract '.'
+	ori $t5,$zero,0x3F			# caract '?'
+	ori $t6,$zero,0x20			# caract ' '
+	ori $s1,$zero,0x2C			# caract ','
+	ori $s2,$zero,0x3B			# caract ';'
+	ori $s3,$zero,0x0A			# caract '\n'
+	
+	# si ponctuation ou espace collé, on les ignore et on lie de nouveau un entier
+	for_3_f7:
+	bge $t1,$t2,fin_for_3_f7
+	#if caractère_précédent = alors on lie un nouvelle entier
+	bne $s7,$t3,suite_for_3_f7		# si != '!'
+	bne $s7,$t4,suite_for_3_f7		# si != '.'
+	bne $s7,$t5,suite_for_3_f7		# si != '?'
+	bne $s7,$t6,suite_for_3_f7		# si != ' '
+	bne $s7,$s1,suite_for_3_f7		# si != ','
+	bne $s7,$s2,suite_for_3_f7		# si != ';'
+	bne $s7,$s3,suite_for_3_f7		# si != '\n'
+	
+	# lecture caractère (fgetc)
+        li $v0,14				# code service lecture fichier
+        # chargement des paramètres
+        ori $a0,$s6,0				# adresse fichier
+        la $a1,buffer				# adresse buffer
+        ori $a2,$zero,1				# nombre de caractère lue
+        syscall
+        ori $s7,$a1,0				# copie du caractère lu dans Caractère_précédent
+	
+	suite_for_3_f7:
+	addi $t1,$t1,1				# incrémentation 3eme for
+	j for_3_f7
+	fin_for_3_f7:
+	
+	#ori $a0,$a1,0				# affichage du caractère
+        #ori $v0,$zero,4
+        #syscall
+        
+	while_1_f7:
+	ori $t3,$zero,0x21			# caract '!'
+	ori $t4,$zero,0x2E			# caract '.'
+	ori $t5,$zero,0x3F			# caract '?'
+	ori $t6,$zero,0x20			# caract ' '
+	ori $s1,$zero,0x2C			# caract ','
+	ori $s2,$zero,0x3B			# caract ';'
+	ori $s3,$zero,0x0A			# caract '\n'
+	
+	beq $s7,$t3,fin_while_1_f7		# si = '!'
+	beq $s7,$t4,fin_while_1_f7		# si = '.'
+	beq $s7,$t5,fin_while_1_f7		# si = '?'
+	beq $s7,$t6,fin_while_1_f7		# si = ' '
+	beq $s7,$s1,fin_while_1_f7		# si = ','
+	beq $s7,$s2,fin_while_1_f7		# si = ';'
+	beq $s7,$s3,fin_while_1_f7		# si = '\n'
+	
+	# lecture caractère (fgetc)
+        li $v0,14				# code service lecture fichier
+        # chargement des paramètres
+        ori $a0,$s6,0				# adresse fichier
+        la $a1,buffer				# adresse buffer
+        ori $a2,$zero,1				# nombre de caractère lue
+        syscall   
+        
+        ori $s0,$v0,0				# si V0= 0 => fin du fichier
+        beq $s0,$zero,fin_while_1_f7		# on sort si c'est le dernier caractère
+        
+        ori $s5,$a1,0				# copie du caractère lu dans caractère_actuel
+        la $t3,loop				
+        lw $t4,0($t3)
+        ori $t5,$zero,4
+        mul $t5,$t4,$t5				# 4*loop
+        
+        la $t6,temp
+       	add $s1,$t6,$t5				# adresse de temp + 4*loop
+        lw $s2,0($s1)				# valeur de tempchar à l'adresse 4*loop
+        addi $s2,$s7,0				# temp[loop] = caractere_actuel
+        sw $s2,0($s1)
+        ori $s7,$s5,0				# caractere_precedent = caractere_actuel
+        
+        addi $t4,$t4,1				# loop ++
+        sw $t4,0($t3)
+        
+        ori $a0,$a1,0				# affichage du caractère
+        ori $v0,$zero,4
+        syscall
+        
+	j while_1_f7
+   	
+	fin_while_1_f7:
+
+	la $a0,texte_while			#afficher le texte nbr lignes
+    	ori $v0,$zero,4
+        syscall
+
+
+	#ATTENTION S4 UTILISE POUR LE FICHIER2
+
+	# On re-ouvre le fichier (fichier2= fopen(..))	
+  	li  $v0, 13            	 		# syscall pour l'ouverture du fichier
+        la  $a0,chemin_texte2  			# chemin du fichier
+        li  $a1,0        	 		# lecture = 0 (flag)
+        li  $a2,0        	 		# = 0 (mode : reading from standard input)
+        syscall        		 		# ouverture du fichier
+	move $s4,$v0      			# sauvegarde adresse fichier 
+	
+        # erreur d'ouverture du fichier si $vo est négatif
+        bltz $v0,erreur_ouverture_part3		# redirection fin du programme
+        #la $a0,success_ouverture_fichier3	# affichage du texte
+   	#ori $v0,$zero,4				
+        #syscall
+        
+
+        # ATTENTION $t9 RESERVE POUR 2EME GRANDE BOUCLE FOR
+        
+        ori $t9,$zero,0				# initialisation i alias irr (code C)
+	#1er for
+	grand_tour_2_f7:
+	bge $t9,$t7,fin_grand_tour_2_f7
+	
+	#2ème for = initialisation de temp_char
+	la $t6,temp_char			# chargement du tableau temp
+	ori $t1,$zero,0				# initialisation i alias i2 (code C)
+	addi $t5,$zero,28			# nb éléments de temp = 28
+	addi $t2,$zero,4			# valeur 4
+	
+	initialisation_temp_char_f7:
+	bge $t1,$t5,fin_initialisation_temp_char_f7
+	mul $t3,$t2,$t1				# 4*i
+	add $t4,$t6,$t3				# récupère adresse temp[i]
+	lw $s1,0($t4)				# récupère valeur de l'adresse temp[i]
+	ori $s1,$zero,0				# initialisation à zéro
+	sw $s1,0($t4)
+	addi $t1,$t1,1				# incrémentation 2ème for init temp
+	j initialisation_temp_char_f7		# retour 2ème for alias	
+	
+	fin_initialisation_temp_char_f7:	# fin initialisation temp
+	# initialisation loop
+	la $t6,loop				# chargement de l'entier loop
+	lw $s1,0($t6)
+	ori $s1,$zero,0				# initialisation loop à zéro
+	sw $s1,0($t6)	
+			
+	# lecture caractère
+        li $v0,14				# code service lecture fichier
+        
+        # chargement des paramètres
+        ori $a0,$s6,0				# adresse fichier
+        la $a1,buffer				# adresse buffer
+        ori $a2,$zero,1				# nombre de caractère lue
+        syscall
+        
+        ori $s7,$v0,0				# copie du caractère lu dans Caractère_précédent
+        
+       	# 3ème for
+	ori $t1,$zero,0				# initialisation i alias i (code C)	
+	addi $t2,$zero,4			# valeur 4
+	
+	ori $t3,$zero,0x21			# caract '!'
+	ori $t4,$zero,0x2E			# caract '.'
+	ori $t5,$zero,0x3F			# caract '?'
+	ori $t6,$zero,0x20			# caract ' '
+	ori $s1,$zero,0x2C			# caract ','
+	ori $s2,$zero,0x3B			# caract ';'
+	ori $s3,$zero,0x0A			# caract '\n'
+	
+	# si ponctuation ou espace collé, on les ignore et on lie de nouveau un entier
+	for_3_partie2_f7:
+	bge $t1,$t2,fin_for_3_partie2_f7
+	#if caractère_précédent = alors on lie un nouvelle entier
+	bne $s7,$t3,suite_for_3_partie2_f7	# si != '!'
+	bne $s7,$t4,suite_for_3_partie2_f7	# si != '.'
+	bne $s7,$t5,suite_for_3_partie2_f7	# si != '?'
+	bne $s7,$t6,suite_for_3_partie2_f7	# si != ' '
+	bne $s7,$s1,suite_for_3_partie2_f7	# si != ','
+	bne $s7,$s2,suite_for_3_partie2_f7	# si != ';'
+	bne $s7,$s3,suite_for_3_partie2_f7	# si != '\n'
+	
+	# lecture caractère (fgetc)
+        li $v0,14				# code service lecture fichier
+        # chargement des paramètres
+        ori $a0,$s6,0				# adresse fichier
+        la $a1,buffer				# adresse buffer
+        ori $a2,$zero,1				# nombre de caractère lue
+        syscall
+        ori $s7,$a1,0				# copie du caractère lu dans Caractère_précédent
+	
+	suite_for_3_partie2_f7:
+	addi $t1,$t1,1				# incrémentation 3eme for
+	j for_3_partie2_f7
+	fin_for_3_partie2_f7:
+	
+	while_2_f7:
+	ori $t3,$zero,0x21			# caract '!'
+	ori $t4,$zero,0x2E			# caract '.'
+	ori $t5,$zero,0x3F			# caract '?'
+	ori $t6,$zero,0x20			# caract ' '
+	ori $s1,$zero,0x2C			# caract ','
+	ori $s2,$zero,0x3B			# caract ';'
+	ori $s3,$zero,0x0A			# caract '\n'
+	
+	beq $s7,$t3,fin_while_2_f7		# si = '!'
+	beq $s7,$t4,fin_while_2_f7		# si = '.'
+	beq $s7,$t5,fin_while_2_f7		# si = '?'
+	beq $s7,$t6,fin_while_2_f7		# si = ' '
+	beq $s7,$s1,fin_while_2_f7		# si = ','
+	beq $s7,$s2,fin_while_2_f7		# si = ';'
+	beq $s7,$s3,fin_while_2_f7		# si = '\n'
+	
+	# lecture caractère (fgetc)
+        li $v0,14				# code service lecture fichier
+        # chargement des paramètres
+        ori $a0,$s6,0				# adresse fichier
+        la $a1,buffer				# adresse buffer
+        ori $a2,$zero,1				# nombre de caractère lue
+        syscall   
+        
+        ori $s0,$v0,0				# si V0= 0 => fin du fichier
+        beq $s0,$zero,fin_while_2_f7		# on sort si c'est le dernier caractère
+        
+        ori $s5,$a1,0				# copie du caractère lu dans caractère_actuel
+        la $t3,loop				
+        lw $t4,0($t3)
+        ori $t5,$zero,4
+        mul $t5,$t4,$t5				# 4*loop
+        
+        la $t6,temp_char
+       	add $s1,$t6,$t5				# adresse de tempchar + 4*loop
+        lw $s2,0($s1)				# valeur de tempchar à l'adresse 4*loop
+        addi $s2,$s7,0				# temp[loop] = caractere_actuel
+        sw $s2,0($s1)
+        ori $s7,$s5,0				# caractere_precedent = caractere_actuel
+        
+        addi $t4,$t4,1				# loop ++
+        sw $t4,0($t3)
+        
+        #ori $a0,$a1,0				# affichage du caractère
+        #ori $v0,$zero,4
+        #syscall
+
+	j while_2_f7
+        
+        fin_while_2_f7:
+        
+        # for 4 pour strcmp
+        la $t6,temp				# chargement du tableau temp
+        la $t4,temp_char			
+	ori $t1,$zero,0				# initialisation i alias i2 (code C)
+	addi $t5,$zero,28			# nb éléments de temp = 28
+	addi $t2,$zero,4			# valeur 4
+        
+        ori $t8,$zero,0				# initialisation comp
+        
+        comparaison_temp_f7:
+	bge $t1,$t5,fin_comparaison_temp_f7
+	mul $t3,$t2,$t1				# 4*i
+	
+	add $t6,$t6,$t3				# récupère adresse temp[i]
+	lw $s1,0($t6)				# récupère valeur de l'adresse temp[i]
+	
+	add $t4,$t4,$t3				# récupère adresse temp_char[i]
+	lw $s2,0($t4)				# récupère valeur de l'adresse temp_char[i]
+	
+	bne $s1,$s2,suite_comparaison_temp_f7
+	addi $t8,$t8,1				# comp ++
+	
+
+	suite_comparaison_temp_f7:
+	addi $t1,$t1,1				# incrémentation
+	j comparaison_temp_f7			
+	
+	fin_comparaison_temp_f7:		
+        
+        bne $t8,$t5,comp_pas_egal_28_1_f7
+        la $t5,temp_int				# on récupère adrese temp-int
+        lw $t8,0($t5)
+        addi $t8,$t8,1
+        sw $t8,0($t5)
+        
+        comp_pas_egal_28_1_f7:
+        
+        addi $t9,$t9,1				# incrémentation 2eme grand for
+	j grand_tour_2_f7			# retour sur le 2eme for
+	fin_grand_tour_2_f7:
+	
+        
+        
+        #printf et close fichier 2
+        j fin_programme_part3
+        
+	erreur_ouverture_part3:	
+	la $a0,erreur_ouverture_fichier3	# affichage du texte
+	ori $v0,$zero,4				
+        syscall
+
+	fin_programme_part3:
+	li   $v0, 16       			# syscall pour fermer le fichier
+  	move $a0, $s4     			# adresse fichier dans $a0 pour fermeture
+  	syscall            			# fermeture
+	
+	
+	
+
+
+
 	addi $t0,$t0,1				# incrémentation 1er for
 	j grand_tour_f7				# retour sur le 1er for alias grand_tour
 	fin_programme_f7:
